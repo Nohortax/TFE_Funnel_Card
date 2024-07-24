1cf7c5fba4030aae9b6d59d85c4cc2045b093d2f
nhx <lucier12@hotmail.com>
2024-07-24 19:51:18 +0200

    Configure Tailwind page and CSS page
    Fonts, colors and more added in the Tailwind page and the CSS page for repetitive use in different vue pages.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,39 @@ export default {
 
     theme: {
         extend: {
-            fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            colors: {
+                // Backgrounds colors
+                bgPrimary: 'rgb(213, 235, 255)',
+                bgSecondary: 'rgb(255, 253, 253)',
+        
+                // Flaschards and inputs background
+                fcColor: 'rgb(246, 251, 255)',
+                bgInput2: 'rgb(246, 251, 255)',
+        
+                // Text color
+                txtColor: 'rgb(0, 10, 12)',
+        
+                // Cards, buttons, ...
+                primary: 'rgb(1, 40, 154)',
+                stroke: 'rgb(1, 40, 154, .1)',
             },
+              
+            fontFamily: {
+              tillana: ['tillana'],
+              raleway: ['raleway'],
+            }
         },
+        boxShadow: {
+          'buttonShadow': '0px 1px 3px 0px slategrey',
+        },
+        screens: {
+          'sp': '320px',
+          'sm': '640px',
+          'md': '768px',
+          'lg': '1024px',
+          'xl': '1280px',
+          '2xl': '1536px'
+        }
     },
 
     plugins: [forms],
