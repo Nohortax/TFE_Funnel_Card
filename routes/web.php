50332c8b3ad39aabf12f2f92eb32a17114746020
nhx <lucier12@hotmail.com>
2024-07-26 17:04:43 +0200

    Name the routes and redirect to index
    Give a name to the routes
    Make the buttons Login and Register redirect to the Index page if authenticated

--- routes/web.php
+++ routes/web.php
@@ -5,27 +5,32 @@ use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
+// FIRST PAGE
 Route::get('/', function () {
     return Inertia::render('App', [
-        // 'canLogin' => Route::has('login'),
-        // 'canRegister' => Route::has('register'),
-        // 'laravelVersion' => Application::VERSION,
-        // 'phpVersion' => PHP_VERSION,
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
     ]);
 });
 
+// INDEX
 Route::get('/decks', function() {
     return Inertia::render('Decks/Index');
-});
+})->middleware(['auth', 'verified'])->name('index');
 
+// SHOW
 Route::get('/cards', function() {
     return Inertia::render('Decks/Show');
-});
+})->name('show');
 
+// DASHBOARD
 Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+// PROFILE/ACCOUNT EDITING
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
